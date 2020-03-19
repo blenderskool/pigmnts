623b72acc3fff40614c8aa387f6878776f89cbcf
blenderskool <akash.hamirwasia@gmail.com>
2020-03-19 21:20:47 +0530

    Add quiet mode
    Quiet mode suppresses the normal output and only shows the color
    codes of the resulting palette.
    It is available under `-q`, `--quiet` or `--silent` flag.

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,37 @@ use std::time::Instant;
 use image::GenericImageView;
 use pigmnts::{Pixels, color::{LAB, RGB}, weights, pigments_pixels};
 
+/// Creates a color palette from image
+/// 
+/// Image is loaded from `image_path` and a palette of `count` colors are created
+fn pigmnts(image_path: &str, count: u8) -> (Vec<(LAB, f32)>, u128) {
+  let img = image::open(image_path)
+    .unwrap()
+    .resize(800, 800, image::imageops::FilterType::CatmullRom);
+  let mut pixels: Pixels = Vec::new();
+
+  // Start a timer
+  let now = Instant::now();
+
+  for (_, _, pix) in img.pixels() {
+    pixels.push(LAB::from(
+      &RGB {
+        r: pix[0],
+        g: pix[1],
+        b: pix[2],
+      }
+    ));
+  }
+
+  let weightfn = weights::resolve_mood(&weights::Mood::Dominant);
+  let mut output = pigments_pixels(&pixels, count, weightfn);
+
+  // Sort the output colors based on dominance
+  output.sort_by(|(_, a), (_, b)| b.partial_cmp(a).unwrap());
+
+  return (output, now.elapsed().as_millis());
+}
+
 fn main() {
 
   let matches = App::new("Pigmnts")
@@ -30,66 +61,67 @@ fn main() {
                         .value_name("FILE")
                         .required(true)
                         .index(1))
+                  .arg(Arg::with_name("quiet")
+                        .short("q")
+                        .long("quiet")
+                        .visible_alias("silent")
+                        .help("Suppress the normal output"))
                   .get_matches();
 
   let image_path = matches.value_of("INPUT").unwrap();
   let count = value_t!(matches, "count", u8).unwrap_or(5);
+  let is_quiet = matches.is_present("quiet");
 
-  print!("{}{}Creating a palette of ", color::Fg(color::White), style::Bold);
-  print!("{}{} ", color::Fg(color::Blue), count);
-  print!("{}colors from ", color::Fg(color::White));
-  println!(
-    "{}{}{}",
-    color::Fg(color::Blue),
-    Path::new(image_path)
-      .file_stem()
-      .unwrap()
-      .to_str()
-      .unwrap(),
-    style::Reset
-  );
-  
-  // Show the spinner in the terminal
-  let sp = Spinner::new(Spinners::Dots, String::default());
 
-  let img = image::open(image_path)
-    .unwrap()
-    .resize(800, 800, image::imageops::FilterType::CatmullRom);
-  let mut pixels: Pixels = Vec::new();
+  if is_quiet {
+    // Quiet mode only shows the color codes in resulting palette
 
-  let now = Instant::now();
-  for (_, _, pix) in img.pixels() {
-    pixels.push(LAB::from(
-      &RGB {
-        r: pix[0],
-        g: pix[1],
-        b: pix[2],
-      }
-    ));
+    let (result, _) = pigmnts(image_path, count);
+
+    for (color, _) in result.iter() {
+      let rgb: RGB = RGB::from(color);
+      println!("{}", rgb.hex());
+    }
+
+  } else {
+
+    print!("{}{}Creating a palette of ", color::Fg(color::White), style::Bold);
+    print!("{}{} ", color::Fg(color::Blue), count);
+    print!("{}colors from ", color::Fg(color::White));
+    println!(
+      "{}{}{}",
+      color::Fg(color::Blue),
+      Path::new(image_path)
+        .file_stem()
+        .unwrap()
+        .to_str()
+        .unwrap(),
+      style::Reset
+    );
+
+    // Show the spinner in the terminal
+    let sp = Spinner::new(Spinners::Dots, String::default());
+    let (result, time) = pigmnts(image_path, count);
+    // Stop the spinner
+    sp.stop();
+
+    println!();
+    for (color, dominance) in result.iter() {
+      let rgb: RGB = RGB::from(color);
+      print!("{}  {} ", color::Bg(color::Rgb(rgb.r, rgb.g, rgb.b)), style::Reset);
+      print!("{}{}{} ", style::Bold, rgb.hex(), style::Reset);
+      println!("--- {}%", dominance * 100.0);
+    }
+    println!();
+
+    println!(
+      "{}{}✓ Success!{} Took {}ms",
+      color::Fg(color::Green),
+      style::Bold,
+      style::Reset,
+      time
+    );
+
   }
 
-  let weightfn = weights::resolve_mood(&weights::Mood::Dominant);
-  let mut output = pigments_pixels(&pixels, count, weightfn);
-
-  // Sort the output colors based on dominance
-  output.sort_by(|(_, a), (_, b)| b.partial_cmp(a).unwrap());
-
-  // Stop the spinner
-  sp.stop();
-
-  println!();
-  for (color, dominance) in output.iter() {
-    let rgb: RGB = RGB::from(color);
-    print!("{}  {} ", color::Bg(color::Rgb(rgb.r, rgb.g, rgb.b)), style::Reset);
-    print!("{}{}{} ", style::Bold, rgb.hex(), style::Reset);
-    println!("--- {}%", dominance * 100.0);
-  }
-
-  println!(
-    "{}{}✓ Success!{} Took {}ms",
-    color::Fg(color::Green),
-    style::Bold,
-    style::Reset,
-    now.elapsed().as_millis()
-  );
 }
