117783e39a13b98b7e210d36449a0dda216de1ab
blenderskool <akash.hamirwasia@gmail.com>
2020-03-19 23:40:24 +0530

    Add multiple files support
    Multiple files can now be passed as input. `--count` also accepts
    multiple values for each input file specified.
    Example, command like `pigmnts file1.jpg -c 3 file2.jpg -c 6` will
    create palette of 3 colors from file1.jpg and 6 colors from
    file2.jpg
    Fixes #10

--- src/main.rs
+++ src/main.rs
@@ -55,11 +55,14 @@ fn main() {
                         .long("count")
                         .value_name("COUNT")
                         .help("Number of colors in the palette")
+                        .multiple(true)
+                        .number_of_values(1)
                         .takes_value(true))
-                  .arg(Arg::with_name("INPUT")
+                  .arg(Arg::with_name("input")
                         .help("Sets the input file to use")
                         .value_name("FILE")
                         .required(true)
+                        .multiple(true)
                         .index(1))
                   .arg(Arg::with_name("quiet")
                         .short("q")
@@ -68,60 +71,72 @@ fn main() {
                         .help("Suppress the normal output"))
                   .get_matches();
 
-  let image_path = matches.value_of("INPUT").unwrap();
-  let count = value_t!(matches, "count", u8).unwrap_or(5);
+  let image_paths = matches.values_of("input").unwrap();
+  let mut counts = values_t!(matches, "count", u8).unwrap_or_else(|e| e.exit());
   let is_quiet = matches.is_present("quiet");
 
-
-  if is_quiet {
-    // Quiet mode only shows the color codes in resulting palette
-
-    let (result, _) = pigmnts(image_path, count);
-
-    for (color, _) in result.iter() {
-      let rgb: RGB = RGB::from(color);
-      println!("{}", rgb.hex());
+  // Fill the default count value (5) for every input file if not specified
+  loop {
+    let diff: i8 = image_paths.len() as i8 - counts.len() as i8;
+    if diff > 0 {
+      counts.push(5);
+    } else {
+      break;
     }
+  }
 
-  } else {
+  // Enumerate through each image_path and generate palettes
+  for (i, image_path) in image_paths.enumerate() {
+  
+    if is_quiet {
+      // Quiet mode only shows the color codes in resulting palette
 
-    print!("{}{}Creating a palette of ", color::Fg(color::White), style::Bold);
-    print!("{}{} ", color::Fg(color::Blue), count);
-    print!("{}colors from ", color::Fg(color::White));
-    println!(
-      "{}{}{}",
-      color::Fg(color::Blue),
-      Path::new(image_path)
-        .file_stem()
-        .unwrap()
-        .to_str()
-        .unwrap(),
-      style::Reset
-    );
+      let (result, _) = pigmnts(image_path, counts[i]);
+      for (color, _) in result.iter() {
+        let rgb: RGB = RGB::from(color);
+        println!("{}", rgb.hex());
+      }
 
-    // Show the spinner in the terminal
-    let sp = Spinner::new(Spinners::Dots, String::default());
-    let (result, time) = pigmnts(image_path, count);
-    // Stop the spinner
-    sp.stop();
+    } else {
 
-    println!();
-    for (color, dominance) in result.iter() {
-      let rgb: RGB = RGB::from(color);
-      print!("{}  {} ", color::Bg(color::Rgb(rgb.r, rgb.g, rgb.b)), style::Reset);
-      print!("{}{}{} ", style::Bold, rgb.hex(), style::Reset);
-      println!("--- {}%", dominance * 100.0);
+      print!("{}{}Creating a palette of ", color::Fg(color::White), style::Bold);
+      print!("{}{} ", color::Fg(color::Blue), counts[i]);
+      print!("{}colors from ", color::Fg(color::White));
+      println!(
+        "{}{}{}",
+        color::Fg(color::Blue),
+        Path::new(image_path)
+          .file_stem()
+          .unwrap()
+          .to_str()
+          .unwrap(),
+        style::Reset
+      );
+
+      // Show the spinner in the terminal
+      let sp = Spinner::new(Spinners::Dots, String::default());
+      let (result, time) = pigmnts(image_path, counts[i]);
+      // Stop the spinner
+      sp.stop();
+
+      println!();
+      for (color, dominance) in result.iter() {
+        let rgb: RGB = RGB::from(color);
+        print!("{}  {} ", color::Bg(color::Rgb(rgb.r, rgb.g, rgb.b)), style::Reset);
+        print!("{}{}{} ", style::Bold, rgb.hex(), style::Reset);
+        println!("--- {}%", dominance * 100.0);
+      }
+      println!();
+
+      println!(
+        "{}{}✓ Success!{} Took {}ms",
+        color::Fg(color::Green),
+        style::Bold,
+        style::Reset,
+        time
+      );
+
     }
-    println!();
-
-    println!(
-      "{}{}✓ Success!{} Took {}ms",
-      color::Fg(color::Green),
-      style::Bold,
-      style::Reset,
-      time
-    );
-
   }
 
 }
